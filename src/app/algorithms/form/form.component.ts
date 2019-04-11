0a6b43b6bda31f0ba38c36ff0c00e1f800bd3b53
Yu Chen Wu <flyinglimao@aol.com>
2019-04-11 08:56:01 +0800

    update algorithm form - now input state and display state

--- src/app/algorithms/form/form.component.ts
+++ src/app/algorithms/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-algorithm-form',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class AlgorithmFormComponent implements OnInit {
+  @Input()
+  editing: boolean;
 
   constructor() { }
 
--- src/app/news/info/info.component.ts
+++ src/app/news/info/info.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class NewsInfoComponent implements OnInit {
   @Input()
-  post: object;
+  post: any;
   constructor() { }
 
   ngOnInit() {
